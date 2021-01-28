9d3e6e52d6025c1d3e63c7f9f40fa8b6a7fc7829
agricola <demophobos@gmail.com>
2021-01-28 22:50:49 +0300

    layout

--- src/app/public/search/components/search-result/search-result.component.ts
+++ src/app/public/search/components/search-result/search-result.component.ts
@@ -7,6 +7,8 @@ import { MatSort } from '@angular/material/sort';
 import { Language } from '@shared/enums';
 import { takeUntil } from 'rxjs/operators';
 import { AppConfig } from '@shared/constants';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
@@ -24,17 +26,39 @@ export class SearchResultComponent extends BaseComponent implements AfterViewIni
 
   limit: number;
 
-  displayedColumns: string[] = ['chunk'];
+  displayedColumns: string[] = ['select', 'chunk'];
 
-  chunks: ChunkElementView[];
+  chunks: MatTableDataSource<ChunkElementView>;
 
   isLoading: boolean = false;
 
   pageEvent: PageEvent;
 
-
+  selection = new SelectionModel<ChunkElementView>(true, []);
+  
   constructor(private searchService: SearchService) {
     super();
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.chunks.data.length;
+    return numSelected === numRows;
+  }
+
+
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.chunks.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: ChunkElementView): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id}`;
   }
 
   ngAfterViewInit() {
@@ -55,7 +79,7 @@ export class SearchResultComponent extends BaseComponent implements AfterViewIni
       });
 
     this.searchService.elementedChunks.pipe(takeUntil(this.destroyed)).subscribe((chunks) => {
-        this.chunks = chunks;
+        this.chunks = new MatTableDataSource(chunks);
       });
 
     this.searchService.isLoading.pipe(takeUntil(this.destroyed)).subscribe((isLoading) => {
@@ -71,7 +95,7 @@ export class SearchResultComponent extends BaseComponent implements AfterViewIni
   }
 
   showPaginator(): boolean{
-    return !this.isLoading && this.chunks !== undefined && this.chunks.length > 0;
+    return !this.isLoading && this.chunks !== undefined && this.chunks.data.length > 0;
   }
 
   loadPage(page: PageEvent): any {
