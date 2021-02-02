82ee0d89a9a923191646c36ae11bccffb4472cf9
agricola <demophobos@gmail.com>
2021-02-02 22:39:43 +0300

    layout changes

--- src/app/public/search/components/condition/condition-panel/condition-panel.component.ts
+++ src/app/public/search/components/condition/condition-panel/condition-panel.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '@shared/components';
+import { ChunkQuery } from '@shared/models';
+import { SearchService } from 'app/public/search/services/search.service';
+import { takeUntil } from 'rxjs/operators';
+import { SearchPageComponent } from '../../search-page/search-page.component';
 
 @Component({
   selector: 'app-condition-panel',
   templateUrl: './condition-panel.component.html',
   styleUrls: ['./condition-panel.component.scss']
 })
-export class ConditionPanelComponent implements OnInit {
-
-  constructor() { }
+export class ConditionPanelComponent extends BaseComponent implements OnInit {
+  query: ChunkQuery;
+  constructor(private searchService: SearchService) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.searchService.chunkQuery.pipe(takeUntil(this.destroyed)).subscribe(query=>{
+      this.query = query;
+    })
   }
 
 }
