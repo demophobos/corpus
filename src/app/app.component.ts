93e2864d3f82dd44ad0e7735744f92c6a2b3e027
agricola <demophobos@gmail.com>
2021-04-13 02:19:31 +0300

    CLR-52

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,27 +21,15 @@ export class AppComponent extends BaseComponent implements OnInit {
   }
   private RegisterIcons() {
     this.matIconRegistry.addSvgIcon(
-      `select_text`,
+      `menu`,
         this.domSanitizer.bypassSecurityTrustResourceUrl(
-          '../assets/icons/select_text.svg'
+          '../assets/icons/menu.svg'
         )
       );
-      this.matIconRegistry.addSvgIcon(
-        `select_category`,
-          this.domSanitizer.bypassSecurityTrustResourceUrl(
-            '../assets/icons/select_category.svg'
-          )
-        );
     this.matIconRegistry.addSvgIcon(
       `logo`,
         this.domSanitizer.bypassSecurityTrustResourceUrl(
           '../assets/icons/logo.svg'
-        )
-      );
-    this.matIconRegistry.addSvgIcon(
-      `text`,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          '../assets/icons/text.svg'
         )
       );
     this.matIconRegistry.addSvgIcon(
@@ -50,64 +38,10 @@ export class AppComponent extends BaseComponent implements OnInit {
           '../assets/icons/research.svg'
         )
       );
-      this.matIconRegistry.addSvgIcon(
-        `forward`,
-          this.domSanitizer.bypassSecurityTrustResourceUrl(
-            '../assets/icons/forward.svg'
-          )
-        );
-        this.matIconRegistry.addSvgIcon(
-          `backward`,
-            this.domSanitizer.bypassSecurityTrustResourceUrl(
-              '../assets/icons/backward.svg'
-            )
-          );
-    this.matIconRegistry.addSvgIcon(
-      `arrow_left`,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          '../assets/icons/arrow_left.svg'
-        )
-      );
-      this.matIconRegistry.addSvgIcon(
-        `arrow_right`,
-          this.domSanitizer.bypassSecurityTrustResourceUrl(
-            '../assets/icons/arrow_right.svg'
-          )
-        );
-    this.matIconRegistry.addSvgIcon(
-      `morph-condition`,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          '../assets/icons/morph-condition.svg'
-        )
-      );
-    this.matIconRegistry.addSvgIcon(
-      `work-condition`,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          '../assets/icons/work-condition.svg'
-        )
-      );
-    this.matIconRegistry.addSvgIcon(
-    `checked`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/checked.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `unchecked`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/unchecked.svg'
-      )
-    );
     this.matIconRegistry.addSvgIcon(
       `filter_plus`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/filter_plus.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `filter_remove`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/filter_remove.svg'
       )
     );
     this.matIconRegistry.addSvgIcon(
@@ -123,27 +57,9 @@ export class AppComponent extends BaseComponent implements OnInit {
       )
     );
     this.matIconRegistry.addSvgIcon(
-      `export`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/export.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
       `info`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/info.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `books`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/books.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `notae`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/notae.svg'
       )
     );
     this.matIconRegistry.addSvgIcon(
@@ -153,118 +69,9 @@ export class AppComponent extends BaseComponent implements OnInit {
       )
     );
     this.matIconRegistry.addSvgIcon(
-      `comment_open`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/comment_open.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `comment_close`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/comment_close.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
       `versio`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/versio.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `rus`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/rus.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `lat`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/lat.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `translate`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/translate.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `options`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/options.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `play`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/play.svg'
-      )
-    );
-
-    this.matIconRegistry.addSvgIcon(
-      `share`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/share.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `taxonomy`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/taxonomy.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `add_text`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/add_text.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `add_index`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/add_index.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `chevron_right`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/chevron_right.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `expand_more`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/expand_more.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `delete`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/delete.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `add`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/add.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `project_folder`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/project_folder.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `folder_open`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/folder_open.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `login`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/login.svg'
       )
     );
     this.matIconRegistry.addSvgIcon(
@@ -280,60 +87,6 @@ export class AppComponent extends BaseComponent implements OnInit {
       )
     );
     this.matIconRegistry.addSvgIcon(
-      `menu`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/menu.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `more_vert`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/more_vert.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `admin`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/admin.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `edit`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/edit.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `project`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/project.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `user`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/user.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `role`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/role.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `perm`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/perm.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `dashboard`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/dashboard.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
       `home`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/home.svg'
@@ -343,12 +96,6 @@ export class AppComponent extends BaseComponent implements OnInit {
       `search`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         '../assets/icons/search.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      `settings`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/icons/settings.svg'
       )
     );
     this.matIconRegistry.addSvgIcon(
