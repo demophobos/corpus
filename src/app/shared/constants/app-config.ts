ed3ef5b18bce0b1a528093b66fc2ef58b6218a58
argicola <argicola.piger@yandex.ru>
2022-05-21 04:59:06 +0300

    version changed

--- src/app/shared/constants/app-config.ts
+++ src/app/shared/constants/app-config.ts
@@ -6,7 +6,7 @@ export const AppConfig = {
     AppFullTitle: 'CORPUS LATINO-ROSSICUM',
     AppShortTitle: 'CLR',
     Copyright: 'ILS RAS',
-    AppVersion: '0.2.3',
+    AppVersion: '0.2.4',
     DefaultPageLimit: 5,
     DefaultSearchLemma: false,
     DefaultShowCommentPane: false
