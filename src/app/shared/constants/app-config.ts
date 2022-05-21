e1d1539883565de42e3c802a1e4820490a106ae8
argicola <argicola.piger@yandex.ru>
2022-05-21 16:34:00 +0300

    https://github.com/demophobos/corpus/issues/4

--- src/app/shared/constants/app-config.ts
+++ src/app/shared/constants/app-config.ts
@@ -6,7 +6,7 @@ export const AppConfig = {
     AppFullTitle: 'CORPUS LATINO-ROSSICUM',
     AppShortTitle: 'CLR',
     Copyright: 'ILS RAS',
-    AppVersion: '0.2.4',
+    AppVersion: '0.2.5',
     DefaultPageLimit: 5,
     DefaultSearchLemma: false,
     DefaultShowCommentPane: false
