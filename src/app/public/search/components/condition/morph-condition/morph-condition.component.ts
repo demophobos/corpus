e20511ed8fc68cf945c1cab7ac407e99ceb6460f
argicola <argicola.piger@yandex.ru>
2022-05-29 19:44:50 +0300

    formatting

--- src/app/public/search/components/condition/morph-condition/morph-condition.component.ts
+++ src/app/public/search/components/condition/morph-condition/morph-condition.component.ts
@@ -11,7 +11,7 @@ import { SearchService } from '../../../services/search.service';
 @Component({
   selector: 'app-morph-condition',
   templateUrl: './morph-condition.component.html',
-  styleUrls: ['./morph-condition.component.scss']
+  styleUrls: ['./morph-condition.component.scss'],
 })
 export class MorphConditionComponent extends BaseComponent implements OnInit {
   panelOpenState = false;
@@ -26,122 +26,129 @@ export class MorphConditionComponent extends BaseComponent implements OnInit {
   persona: string = TaxonomyCategoryEnum.Person;
   casus: string = TaxonomyCategoryEnum.Case;
   gradus: string = TaxonomyCategoryEnum.Degree;
-  genus2:string = TaxonomyCategoryEnum.Voice;
+  genus2: string = TaxonomyCategoryEnum.Voice;
 
-  constructor(private searchService: SearchService, private commonDataService: CommonDataService, private bottomSheet: MatBottomSheet) {
+  constructor(
+    private searchService: SearchService,
+    private commonDataService: CommonDataService,
+    private bottomSheet: MatBottomSheet
+  ) {
     super();
   }
 
   async ngOnInit() {
-
-    this.commonDataService.getTaxonomyItems().then((items)=>{
+    this.commonDataService.getTaxonomyItems().then((items) => {
       this.categoryItems = items;
     });
 
-    this.searchService.chunkQuery.pipe(takeUntil(this.destroyed)).subscribe(query=>{
-      this.query = query;
-    });
+    this.searchService.chunkQuery
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((query) => {
+        this.query = query;
+      });
   }
 
-  getCategoryTitle(category: string) : string {
-    if(this.categoryItems){
-      return this.categoryItems.filter(i=>i.categoryCode == category)
-      .map(i=>i.categoryDesc)[0];
+  getCategoryTitle(category: string): string {
+    if (this.categoryItems) {
+      return this.categoryItems
+        .filter((i) => i.categoryCode == category)
+        .map((i) => i.categoryDesc)[0];
     }
   }
 
-  getCategoryItems(category: string) : TaxonomyViewModel[]{
-    if(this.categoryItems){
-      return this.categoryItems.filter(i=>i.categoryCode == category)
-      .sort((a, b) => (a.code < b.code ? -1 : 1));
+  getCategoryItems(category: string): TaxonomyViewModel[] {
+    if (this.categoryItems) {
+      return this.categoryItems
+        .filter((i) => i.categoryCode == category)
+        .sort((a, b) => (a.code < b.code ? -1 : 1));
     }
   }
 
-  getSelected(cat: string){
-    if(cat == TaxonomyCategoryEnum.Pos){
+  getSelected(cat: string) {
+    if (cat == TaxonomyCategoryEnum.Pos) {
       return this.query.pos.length;
     }
-    if(cat == TaxonomyCategoryEnum.Tense){
+    if (cat == TaxonomyCategoryEnum.Tense) {
       return this.query.tense.length;
     }
-    if(cat == TaxonomyCategoryEnum.Case){
+    if (cat == TaxonomyCategoryEnum.Case) {
       return this.query.case.length;
     }
-    if(cat == TaxonomyCategoryEnum.Degree){
+    if (cat == TaxonomyCategoryEnum.Degree) {
       return this.query.degree.length;
     }
-    if(cat == TaxonomyCategoryEnum.Gender){
+    if (cat == TaxonomyCategoryEnum.Gender) {
       return this.query.gender.length;
     }
-    if(cat == TaxonomyCategoryEnum.Mood){
+    if (cat == TaxonomyCategoryEnum.Mood) {
       return this.query.mood.length;
     }
-    if(cat == TaxonomyCategoryEnum.Number){
+    if (cat == TaxonomyCategoryEnum.Number) {
       return this.query.number.length;
     }
-    if(cat == TaxonomyCategoryEnum.Person){
+    if (cat == TaxonomyCategoryEnum.Person) {
       return this.query.person.length;
     }
-    if(cat == TaxonomyCategoryEnum.Voice){
+    if (cat == TaxonomyCategoryEnum.Voice) {
       return this.query.voice.length;
     }
   }
 
-  posSelected(selected){
-    if(this.query){
+  posSelected(selected) {
+    if (this.query) {
       this.query.pos = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  tempusSelected(selected){
-    if(this.query){
+  tempusSelected(selected) {
+    if (this.query) {
       this.query.tense = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  modusSelected(selected){
-    if(this.query){
+  modusSelected(selected) {
+    if (this.query) {
       this.query.mood = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  genusSelected(selected){
-    if(this.query){
+  genusSelected(selected) {
+    if (this.query) {
       this.query.gender = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  numerusSelected(selected){
-    if(this.query){
+  numerusSelected(selected) {
+    if (this.query) {
       this.query.number = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  personaSelected(selected){
-    if(this.query){
+  personaSelected(selected) {
+    if (this.query) {
       this.query.person = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  casusSelected(selected){
-    if(this.query){
+  casusSelected(selected) {
+    if (this.query) {
       this.query.case = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  gradusSelected(selected){
-    if(this.query){
+  gradusSelected(selected) {
+    if (this.query) {
       this.query.degree = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  genus2Selected(selected){
-    if(this.query){
+  genus2Selected(selected) {
+    if (this.query) {
       this.query.voice = selected;
       this.searchService.setSelectedMorphAttrubutes(this.query);
     }
   }
-  closeMorphologia(){
-this.bottomSheet.dismiss();
+  closeMorphologia() {
+    this.bottomSheet.dismiss();
   }
 }
